05942ec09ca6cb404589b15f69ca017676df1e15
sudhanshukumar722 <129382440+sudhanshukumar722@users.noreply.github.com>
2024-02-16 06:55:38 +0530

    jason data

--- index.js
+++ index.js
@@ -4,7 +4,40 @@ const app=express()
 
 const port=4001
 
+const data =[
+	{
+		color: "red",
+		value: "#f00"
+	},
+	{
+		color: "green",
+		value: "#0f0"
+	},
+	{
+		color: "blue",
+		value: "#00f"
+	},
+	{
+		color: "cyan",
+		value: "#0ff"
+	},
+	{
+		color: "magenta",
+		value: "#f0f"
+	},
+	{
+		color: "yellow",
+		value: "#ff0"
+	},
+	{
+		color: "black",
+		value: "#000"
+	}
+]
 
+app.get('/github',(req,res)=>{
+    res.json(data)
+})
 app.get('/',(req,res)=>{
     res.send('Hello djdjjdd')
 })
